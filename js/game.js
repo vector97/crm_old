f888f2b919dc6082ad4adbb6a03bd8df796a320d
Viktor Bogatenkov <vector_97@mail.ru>
2022-06-08 17:29:44 +0400

    доп. задание к M04L07

--- js/game.js
+++ js/game.js
@@ -3,26 +3,61 @@
 const background = 'padding: 0 20px; text-shadow: 0 0 10px red; color: green';
 console.log('%c Задача №1 на рекурсию Угадай число', background);
 
-const randomNumber = Math.ceil(Math.random() * 100);
+const min = prompt('Введите минимальное целое число', '');
+const max = prompt('Введите максимальное целое число', '');
+let attempt = Math.ceil((max - min) * 0.3);
+const randomNumber = Math.ceil(Math.random() * (+max - +min + 1)) + +min;
 console.log('randomNumber: ', randomNumber);
+const userNums = [];
 
 const askNumber = () => {
-  const userNumber = prompt('Отгадайте число от 1 до 100', '');
+  while (attempt > 0) {
+    const userNumber =
+      prompt(`Отгадайте число в диапазоне от ${min} до ${max}
+      за ${attempt} попыток`, '');
 
-  if (userNumber === null) {
-    alert('Конец игры!');
-  } else if (+userNumber === randomNumber) {
-    alert('Правильно!');
-  } else if (userNumber > randomNumber) {
-    alert('Меньше!');
-    askNumber();
-  } else if (userNumber < randomNumber) {
-    alert('Больше!');
-    askNumber();
-  } else if (!isFinite(userNumber) || userNumber === '') {
-    alert('Введите число!');
-    askNumber();
+    if (!userNums.includes(userNumber) && isFinite(userNumber)) {
+      userNums.push(userNumber);
+      attempt--;
+    } else {
+      alert('Это число вы уже вводили!');
+      return askNumber();
+    }
+
+    console.log('userNums: ', userNums);
+
+    if (userNumber === null) {
+      alert('Конец игры!');
+      return;
+    }
+
+    if (+userNumber === randomNumber) {
+      alert('Правильно!');
+      return;
+    }
+
+    if (userNumber > randomNumber) {
+      alert('Меньше!');
+      return askNumber();
+    }
+
+    if (userNumber < randomNumber) {
+      alert('Больше!');
+      return askNumber();
+    }
+
+    if (!isFinite(userNumber) || userNumber === '') {
+      alert('Введите число!');
+      return askNumber();
+    }
   }
+
+  alert(`У вас закончились попытки! К сожалению, вы проиграли.
+    Правильное число ${randomNumber}`);
 };
 
-askNumber();
+if (min === null || max === null) {
+  alert('Конец игры!');
+} else {
+  askNumber();
+}
